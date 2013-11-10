3ca620657e59e571f262820042e18b72d6f7fdb5
Tatiana Borisova <tanyatik@yandex.ru>
2013-11-10 23:48:13 +0400

    Important fix in memory manager (add swap for blocks)

--- memory_manager_task.cpp
+++ memory_manager_task.cpp
@@ -7,6 +7,7 @@ struct MemoryBlock {
         
         KeyT(size_t block_size = 0, size_t block_position = 0);
         bool operator < (struct KeyT other);
+        void debugPrint() const;
     };
     size_t size;
     size_t position;
@@ -15,7 +16,7 @@ struct MemoryBlock {
     int free_heap_index; // stores -1 if block is occupied
 
     MemoryBlock();
-    MemoryBlock(size_t size, size_t position, int free_heap_index);
+    MemoryBlock(size_t size, size_t position);
     MemoryBlock(size_t size, 
                 size_t position, 
                 int free_heap_index, 
@@ -23,13 +24,9 @@ struct MemoryBlock {
                 MemoryBlock *p_next);
     KeyT getKey() const;
     bool isOccupied() const;
+    void debugPrint() const;
+    void swap(MemoryBlock &other);
 };
-
-template<>
-struct TraitsSentinel<MemoryBlock::KeyT> {
-    static MemoryBlock::KeyT getMinusSentinel() { return MemoryBlock::KeyT(0, 0); };
-};
-
 
 MemoryBlock::MemoryBlock() :
     size(0),
@@ -38,12 +35,12 @@ MemoryBlock::MemoryBlock() :
     p_next(nullptr),
     free_heap_index(0) {}
 
-MemoryBlock::MemoryBlock(size_t size, size_t position, int free_heap_index) :
+MemoryBlock::MemoryBlock(size_t size, size_t position) :
     size(size),
     position(position),
     p_prev(nullptr), 
     p_next(nullptr),
-    free_heap_index(free_heap_index) {}
+    free_heap_index(-1) {}
 
 MemoryBlock::MemoryBlock(size_t size, 
                          size_t position, 
@@ -64,6 +61,34 @@ bool MemoryBlock::isOccupied() const {
     return free_heap_index == -1;
 }
 
+void MemoryBlock::swap(MemoryBlock &other) {
+    MemoryBlock tmp = *this;
+    *this = other;
+    other = tmp;
+
+    if (p_prev) {
+        p_prev->p_next = this;
+    }
+    if (p_next) {
+        p_next->p_prev = this;
+    }
+
+    if (other.p_prev) {
+        other.p_prev->p_next = &other;
+    }
+    if (other.p_next) {
+        other.p_next->p_prev = &other;
+    }
+}
+
+void swap(MemoryBlock &one, MemoryBlock &other) {
+    one.swap(other);
+}
+
+void MemoryBlock::debugPrint() const {
+    std::cout << "[" << position << ":" << size << ':' << (free_heap_index == -1 ? 'O' : 'F') << "]";
+}
+
 MemoryBlock::KeyT::KeyT(size_t block_size, size_t block_position) :
     block_size(block_size),
     block_position(block_position) {}
@@ -73,23 +98,35 @@ bool MemoryBlock::KeyT::operator <(struct KeyT other) {
             (block_size == other.block_size && block_position < other.block_position));
 }
 
+void MemoryBlock::KeyT::debugPrint() const {
+    std::cout << block_size << ':' << block_position;
+}
+
+template<>
+struct TraitsSentinel<MemoryBlock::KeyT> {
+    static MemoryBlock::KeyT getMinusSentinel() { return MemoryBlock::KeyT(0, 0); };
+};
+
 class MemoryManager {
     public:
         MemoryManager(int memory_cells_number, int max_query_number);
 
         int allocate(int block_size);
         void deallocate(int query_number);
-
+        void debugPrint() const;
     private:
         int memory_cells_number_;
         int query_number_;
         int query_counter_;
+        // debug
 
         std::vector<MemoryBlock> occupied_blocks_;
         typedef BinaryHeap<MemoryBlock::KeyT, MemoryBlock> TFreeBlocksHeap; 
         TFreeBlocksHeap free_blocks_;
+        MemoryBlock head_;
 
-        MemoryBlock& updateFreeBlockLinks(TFreeBlocksHeap::TIndex free_idx);
+        MemoryBlock& getFreeBlockLinks(TFreeBlocksHeap::TIndex free_idx);
+        void connectBlocks(MemoryBlock *one, MemoryBlock *other);
 };
 
 MemoryManager::MemoryManager(int memory_cells_number, int query_number) :
@@ -97,9 +134,11 @@ MemoryManager::MemoryManager(int memory_cells_number, int query_number) :
     query_number_(query_number),
     query_counter_(0),
     occupied_blocks_(query_number),
-    free_blocks_(query_number) {
-    MemoryBlock free_block(memory_cells_number, 0, 0, nullptr, nullptr);
-    free_blocks_.insert(free_block.getKey(), free_block);
+    free_blocks_(query_number),
+    head_(0, 0) {
+    MemoryBlock free_block(memory_cells_number, 0);
+    auto idx = free_blocks_.insert(free_block.getKey(), free_block);
+    connectBlocks(&head_, &free_blocks_[idx]);
 }
 
 int MemoryManager::allocate(int block_size) {
@@ -107,6 +146,7 @@ int MemoryManager::allocate(int block_size) {
         return -1;
     }
     int biggest_block_size = free_blocks_.getMax().size;
+    int position = free_blocks_.getMax().position;
     if (biggest_block_size < block_size) {
         // no place for such a big block
         return -1;
@@ -119,13 +159,11 @@ int MemoryManager::allocate(int block_size) {
         // whole free block turns to occupied block
         MemoryBlock biggest_block = free_blocks_.extractMax();
 
-        MemoryBlock new_occ_block = MemoryBlock(block_size, 
-                                                biggest_block.position, 
-                                                -1, 
-                                                biggest_block.p_prev, 
-                                                biggest_block.p_next);
+        occupied_blocks_[query_counter_] = MemoryBlock(block_size, biggest_block.position);
+        MemoryBlock& new_occ_block = occupied_blocks_[query_counter_];
 
-        occupied_blocks_[query_counter_++] = new_occ_block;
+        connectBlocks(biggest_block.p_prev, &new_occ_block);
+        connectBlocks(&new_occ_block, biggest_block.p_next);
     } else if (biggest_block_size > block_size) {
         // ... |         free        | ...
         //                 ||     
@@ -135,19 +173,20 @@ int MemoryManager::allocate(int block_size) {
         MemoryBlock biggest_block = free_blocks_.extractMax();
 
         // create occupied block
-        MemoryBlock& new_occ_block = occupied_blocks_[query_counter_++] = 
-            MemoryBlock(block_size, biggest_block.position, -1, biggest_block.p_prev, nullptr);
+        occupied_blocks_[query_counter_] = MemoryBlock(block_size, biggest_block.position);
+        MemoryBlock& new_occ_block = occupied_blocks_[query_counter_];
         // create free block
         MemoryBlock tmp_free_block = MemoryBlock(biggest_block.size - block_size, 
-                                                 biggest_block.position + block_size, 
-                                                 -1, 
-                                                 &new_occ_block, 
-                                                 biggest_block.p_next); 
+                                                 biggest_block.position + block_size);
         auto free_idx = free_blocks_.insert(tmp_free_block.getKey(), tmp_free_block);
-        MemoryBlock& new_free_block = updateFreeBlockLinks(free_idx);
+        MemoryBlock& new_free_block = getFreeBlockLinks(free_idx);
 
-        new_occ_block.p_next = &new_free_block;
+        connectBlocks(biggest_block.p_prev, &new_occ_block);
+        connectBlocks(&new_occ_block, &new_free_block);
+        connectBlocks(&new_free_block, biggest_block.p_next);
     }
+    ++query_counter_;
+    return position;
 }
 
 void MemoryManager::deallocate(int query_number) {
@@ -164,8 +203,11 @@ void MemoryManager::deallocate(int query_number) {
         // ... | occ  | free | occ | ...
 
         // creating free block
-        auto free_idx = free_blocks_.insert(occ.getKey(), occ);
-        MemoryBlock& new_free_block = updateFreeBlockLinks(free_idx);
+        MemoryBlock tmp(occ.size, occ.position);
+        auto free_idx = free_blocks_.insert(tmp.getKey(), tmp);
+        MemoryBlock& new_free_block = getFreeBlockLinks(free_idx);
+        connectBlocks(occ.p_prev, &new_free_block);
+        connectBlocks(&new_free_block, occ.p_next);
     } else if (!occ.p_prev->isOccupied() && occ.p_next->isOccupied()) {
         // ... | free |  occ | occ | ...
         //               ||     
@@ -175,13 +217,12 @@ void MemoryManager::deallocate(int query_number) {
         // updating free block
         MemoryBlock& free_left = *occ.p_prev;
 
-        MemoryBlock tmp(free_left.size + occ.size, 
-                        free_left.position,
-                        -1,
-                        free_left.p_prev,
-                        occ.p_next);
+        MemoryBlock tmp(free_left.size + occ.size, free_left.position);
         auto free_idx = free_blocks_.replace(free_left.free_heap_index, tmp.getKey(), tmp);
-        MemoryBlock& new_free_block = updateFreeBlockLinks(free_idx);
+        MemoryBlock& new_free_block = getFreeBlockLinks(free_idx);
+
+        connectBlocks(free_left.p_next, &new_free_block);
+        connectBlocks(&new_free_block, occ.p_next);
     } else if (occ.p_prev->isOccupied() && !occ.p_next->isOccupied()) {
         // ... | occ  |  occ | free | ...
         //               ||     
@@ -191,13 +232,12 @@ void MemoryManager::deallocate(int query_number) {
         // updating free block
         MemoryBlock& free_right = *occ.p_next;
 
-        MemoryBlock tmp(free_right.size + occ.size, 
-                        occ.position,
-                        -1,
-                        occ.p_prev,
-                        free_right.p_next);
+        MemoryBlock tmp(free_right.size + occ.size, occ.position);
         auto free_idx = free_blocks_.replace(free_right.free_heap_index, tmp.getKey(), tmp);
-        MemoryBlock& new_free_block = updateFreeBlockLinks(free_idx);
+        MemoryBlock& new_free_block = getFreeBlockLinks(free_idx);
+
+        connectBlocks(occ.p_prev, &new_free_block);
+        connectBlocks(&new_free_block, free_right.p_next);
     }
     else {
         // ... | free  |  occ | free | ...
@@ -209,32 +249,50 @@ void MemoryManager::deallocate(int query_number) {
         MemoryBlock& free_left = *occ.p_prev;
         MemoryBlock& free_right = *occ.p_next;
 
-        MemoryBlock tmp(free_left.size + occ.size + free_right.size, 
-                        free_left.position, 
-                        -1,
-                        free_left.p_prev, 
-                        free_right.p_prev);
+        MemoryBlock tmp(free_left.size + occ.size + free_right.size, free_left.position);
         auto new_idx = free_blocks_.replace(free_left.free_heap_index, tmp.getKey(), tmp);
-        MemoryBlock& new_free_block = updateFreeBlockLinks(new_idx);
+        MemoryBlock& new_free_block = getFreeBlockLinks(new_idx);
+
+        connectBlocks(free_left.p_prev, &new_free_block);
+        connectBlocks(&new_free_block, free_right.p_next);
         free_blocks_.remove(free_right.free_heap_index);
     }
 }
 
-MemoryBlock& MemoryManager::updateFreeBlockLinks(TFreeBlocksHeap::TIndex free_idx) {
+MemoryBlock& MemoryManager::getFreeBlockLinks(TFreeBlocksHeap::TIndex free_idx) {
     MemoryBlock& new_free_block = free_blocks_[free_idx];
     new_free_block.free_heap_index = free_idx;
-    if (new_free_block.p_prev) {
-        new_free_block.p_prev->p_next = &new_free_block;
+    return new_free_block;
+}
+
+void MemoryManager::connectBlocks(MemoryBlock *one, MemoryBlock *other) {
+    if(one) {
+        one->p_next = other;
     }
-    if (new_free_block.p_next) {
-        new_free_block.p_next->p_prev = &new_free_block;
+    if(other) {
+        other->p_prev = one;
     }
+}
+
+void MemoryManager::debugPrint() const {
+    MemoryBlock *ptr = head_.p_next;
+    while(ptr) {
+        ptr->debugPrint();
+        std::cout << ' ';
+        ptr = ptr->p_next;
+    }
+    std::cout << std::endl;
 }
 
 int main() {
     MemoryManager manager(1000, 4);
+    manager.debugPrint();
     manager.allocate(500);
+    manager.debugPrint();
     manager.allocate(250);
+    manager.debugPrint();
     manager.allocate(125);
-    //manager.deallocate(2);
+    manager.debugPrint();
+    manager.deallocate(2);
+    manager.debugPrint();
 }
