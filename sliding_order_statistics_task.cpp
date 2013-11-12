7d5d0544742c21bb3ba54613c834f0f9c1d12e81
Tatiana Borisova <tanyatik@yandex.ru>
2013-11-12 18:15:50 +0400

    Working version before submitting

--- sliding_order_statistics_task.cpp
+++ sliding_order_statistics_task.cpp
@@ -3,21 +3,22 @@
 #include <vector>
 #include <assert.h>
 
-// Class representing binary heap with user specified elem type and value type
-template <typename TElem, typename TComparator>
+template <typename TElem>
 class BinaryHeap {
     public:
         typedef long long TIndex;
         typedef TElem TElemType;
 
-        BinaryHeap(std::size_t max_size);
-        TElem removeTop();
+        BinaryHeap(std::size_t max_size, 
+                   std::function <bool(TElem&, TElem&)> comparator,
+                   std::function <TElem()> sentinel);
+        TIndex removeTop();
         const TElem& getTop() const;
         TIndex insert(TElem elem);
-        TElem& operator [] (TIndex index);
-        const TElem& operator [] (TIndex index) const;
+        TElem &operator [] (TIndex index);
+        const TElem &operator [] (TIndex index) const;
         TIndex replace(TIndex prev_idx, TElem new_elem);
-        void remove(TIndex idx);
+        TIndex remove(TIndex idx);
         void debugPrint() const;
         unsigned int getSize() const;
     private:
@@ -33,80 +34,85 @@ class BinaryHeap {
         unsigned int heap_size_;
         unsigned int max_heap_size_;
         std::vector<TElem> elems_;
+        std::function <bool(TElem&, TElem&)> comparator_;
+        std::function <TElem()> sentinel_;
 };
 
-template<typename TElem, typename TComparator>
-BinaryHeap<TElem, TComparator>::BinaryHeap(std::size_t max_size) : 
+template<typename TElem>
+BinaryHeap<TElem>::BinaryHeap(std::size_t max_size, 
+                              std::function<bool(TElem&, TElem&)> comparator,
+                              std::function<TElem()> sentinel) : 
     heap_size_(0),
     max_heap_size_(max_size),
-    elems_(max_heap_size_) {}
+    elems_(max_heap_size_),
+    comparator_(comparator),
+    sentinel_(sentinel) {}
 
-template<typename TElem, typename TComparator>
-const TElem& BinaryHeap<TElem, TComparator>::getTop() const{
+template<typename TElem>
+const TElem& BinaryHeap<TElem>::getTop() const{
     assert(heap_size_ >= 1);
-    
+   
     return elems_[0];
 }
 
-template<typename TElem, typename TComparator>
-typename BinaryHeap<TElem, TComparator>::TIndex BinaryHeap<TElem, TComparator>::getParentIndex(TIndex idx) {
+template<typename TElem>
+typename BinaryHeap<TElem>::TIndex BinaryHeap<TElem>::getParentIndex(TIndex idx) {
     assert(idx < max_heap_size_);
     if(idx <= 0) return -1;
     return (idx - 1) >> 1;
 }
 
-template<typename TElem, typename TComparator>
-typename BinaryHeap<TElem, TComparator>::TIndex BinaryHeap<TElem, TComparator>::getLeftIndex(TIndex idx) {
-    assert(idx == 0 || (idx > 0 && idx < heap_size_));
+template<typename TElem>
+typename BinaryHeap<TElem>::TIndex BinaryHeap<TElem>::getLeftIndex(TIndex idx) {
+    assert(idx == 0 || (idx > 0 && idx <= heap_size_));
     return (idx << 1) + 1;
 }
 
-template<typename TElem, typename TComparator>
-typename BinaryHeap<TElem, TComparator>::TIndex BinaryHeap<TElem, TComparator>::getRightIndex(TIndex idx) {
-    assert(idx == 0 || (idx > 0 && idx < heap_size_));
+template<typename TElem>
+typename BinaryHeap<TElem>::TIndex BinaryHeap<TElem>::getRightIndex(TIndex idx) {
+    assert(idx == 0 || (idx > 0 && idx <= heap_size_));
     return (idx << 1) + 2;
 }
 
-template<typename TElem, typename TComparator>
-void BinaryHeap<TElem, TComparator>::swapElements(TIndex a, TIndex b) {
+template<typename TElem>
+void BinaryHeap<TElem>::swapElements(TIndex a, TIndex b) {
     assert(a >= 0 && a < max_heap_size_);
     assert(b >= 0 && b < max_heap_size_);
     using std::swap;
-    swap(elems_[a], elems_[b]);
+    swap_(elems_[a], elems_[b]);
 }
 
-template<typename TElem, typename TComparator>
-unsigned int BinaryHeap<TElem, TComparator>::getSize() const {
+template<typename TElem>
+unsigned int BinaryHeap<TElem>::getSize() const {
     assert(heap_size_ >= 0 && heap_size_ <= max_heap_size_);
     return heap_size_;
 }
 
-template<typename TElem, typename TComparator>
-TElem& BinaryHeap<TElem, TComparator>::operator [] (TIndex idx) {
-    assert(idx >= 0 && idx < heap_size_);
+template<typename TElem>
+TElem& BinaryHeap<TElem>::operator [] (TIndex idx) {
+    assert(idx >= 0 && idx <= heap_size_);
     // just as an experiment
-    return const_cast<TElem&>(static_cast<const BinaryHeap<TElem, TComparator>&>(*this)[idx]);
+    return const_cast<TElem&>(static_cast<const BinaryHeap<TElem>&>(*this)[idx]);
 }
 
-template<typename TElem, typename TComparator>
-const TElem& BinaryHeap<TElem, TComparator>::operator [] (TIndex idx) const {
-    assert(idx >= 0 && idx < heap_size_);
+template<typename TElem>
+const TElem& BinaryHeap<TElem>::operator [] (TIndex idx) const {
+    assert(idx >= 0 && idx <= heap_size_);
     return elems_[idx];
 }
 
-template<typename TElem, typename TComparator>
-void BinaryHeap<TElem, TComparator>::heapifyDown(TIndex idx) {
-    assert(idx == 0 || (idx > 0 && idx < heap_size_));
+template<typename TElem>
+void BinaryHeap<TElem>::heapifyDown(TIndex idx) {
+    assert(idx == 0 || (idx > 0 && idx <= heap_size_));
 
     TIndex left_idx = getLeftIndex(idx);
     TIndex right_idx = getRightIndex(idx);
     TIndex max_idx = idx;
-    TComparator comparator;
 
-    if (left_idx < heap_size_ && comparator(elems_[max_idx], elems_[left_idx])) {
+    if (left_idx < heap_size_ && comparator_(elems_[max_idx], elems_[left_idx])) {
         max_idx = left_idx;
     }
-    if (right_idx < heap_size_ && comparator(elems_[max_idx], elems_[right_idx])) {
+    if (right_idx < heap_size_ && comparator_(elems_[max_idx], elems_[right_idx])) {
         max_idx = right_idx;
     }
     if (max_idx != idx) {
@@ -116,11 +122,11 @@ void BinaryHeap<TElem, TComparator>::heapifyDown(TIndex idx) {
     }
 }
 
-template<typename TElem, typename TComparator>
-typename BinaryHeap<TElem, TComparator>::TIndex BinaryHeap<TElem, TComparator>::insert(TElem elem) {
+template<typename TElem>
+typename BinaryHeap<TElem>::TIndex BinaryHeap<TElem>::insert(TElem elem) {
     assert(heap_size_ + 1 <= max_heap_size_);
 
-    elems_[heap_size_] = TComparator::getMinusSentinel();
+    elems_[heap_size_] = sentinel_();
     TIndex new_idx = increaseKey(elem, heap_size_);
     assert(new_idx >= 0 && new_idx < max_heap_size_);
 
@@ -128,8 +134,8 @@ typename BinaryHeap<TElem, TComparator>::TIndex BinaryHeap<TElem, TComparator>::
     return new_idx;
 }
 
-template<typename TElem, typename TComparator>
-typename BinaryHeap<TElem, TComparator>::TIndex BinaryHeap<TElem, TComparator>::replace(TIndex prev_idx, 
+template<typename TElem>
+typename BinaryHeap<TElem>::TIndex BinaryHeap<TElem>::replace(TIndex prev_idx, 
                                        TElem new_elem) {
     assert(prev_idx >= 0 && prev_idx < heap_size_);
 
@@ -139,18 +145,17 @@ typename BinaryHeap<TElem, TComparator>::TIndex BinaryHeap<TElem, TComparator>::
     return idx;
 }
 
-template<typename TElem, typename TComparator>
-TElem BinaryHeap<TElem, TComparator>::removeTop() {
+template<typename TElem>
+typename BinaryHeap<TElem>::TIndex BinaryHeap<TElem>::removeTop() {
     assert(heap_size_ >= 1);
     
-    TElem max_elem = elems_[0];
     deleteElement(0);
     heapifyDown(0);
-    return max_elem;
+    return heap_size_;
 }
 
-template<typename TElem, typename TComparator>
-void BinaryHeap<TElem, TComparator>::remove(TIndex idx) {
+template<typename TElem>
+typename BinaryHeap<TElem>::TIndex BinaryHeap<TElem>::remove(TIndex idx) {
     assert(idx >= 0 && idx < heap_size_);
     assert(heap_size_ >= 1);
     
@@ -158,28 +163,27 @@ void BinaryHeap<TElem, TComparator>::remove(TIndex idx) {
 
     idx = heapifyUp(idx);
     heapifyDown(idx);
+    return heap_size_;
 }
 
-template<typename TElem, typename TComparator>
-typename BinaryHeap<TElem, TComparator>::TIndex BinaryHeap<TElem, TComparator>::increaseKey(TElem new_elem, TIndex idx) {
-    TComparator comparator;
+template<typename TElem>
+typename BinaryHeap<TElem>::TIndex BinaryHeap<TElem>::increaseKey(TElem new_elem, TIndex idx) {
     assert(idx >= 0 && idx < max_heap_size_);
-    assert(comparator(elems_[idx], new_elem));
+    assert(comparator_(elems_[idx], new_elem));
 
     elems_[idx] = new_elem;
     idx = heapifyUp(idx);
     return idx;
 }
 
-template<typename TElem, typename TComparator>
-void BinaryHeap<TElem, TComparator>::deleteElement(TIndex idx) {
+template<typename TElem>
+void BinaryHeap<TElem>::deleteElement(TIndex idx) {
     swapElements(idx, heap_size_ - 1);
-    elems_[heap_size_ - 1] = TElem();
     --heap_size_;
 }
 
-template<typename TElem, typename TComparator>
-void BinaryHeap<TElem, TComparator>::debugPrint() const {
+template<typename TElem>
+void BinaryHeap<TElem>::debugPrint() const {
     for(auto k_it = elems_.begin(); k_it != elems_.end(); ++k_it) {
         std::cout << *k_it;
         std::cout << '\t';
@@ -187,12 +191,11 @@ void BinaryHeap<TElem, TComparator>::debugPrint() const {
     std::cout << "\n";
 }
 
-template<typename TElem, typename TComparator>
-typename BinaryHeap<TElem, TComparator>::TIndex BinaryHeap<TElem, TComparator>::heapifyUp(TIndex idx) {
+template<typename TElem>
+typename BinaryHeap<TElem>::TIndex BinaryHeap<TElem>::heapifyUp(TIndex idx) {
     TIndex parent_idx = getParentIndex(idx);
-    TComparator comparator;
 
-    while(idx > 0 && comparator(elems_[parent_idx], elems_[idx])) {
+    while(idx > 0 && comparator_(elems_[parent_idx], elems_[idx])) {
         swapElements(idx, parent_idx);
         idx = parent_idx;
         parent_idx = getParentIndex(idx);
@@ -200,83 +203,113 @@ typename BinaryHeap<TElem, TComparator>::TIndex BinaryHeap<TElem, TComparator>::
     return idx;
 }
 
+struct Number;
+
+typedef BinaryHeap<Number *> Heap;
+
 struct Number {
     int number;
-    int order;
+    Heap *heap_ptr;
+    int heap_idx;
 
-    struct ComparatorLess {
-        bool operator () (Number one, Number other) {
-            return (one.number < other.number) || 
-                   (one.number == other.number && one.order < other.order);
-        }
-        static Number getMinusSentinel() {
-            return Number{-1000000000, -1000000000};
-        }
-    };
-
-    struct ComparatorGreat {
-        bool operator () (Number one, Number other) {
-            return (one.number > other.number) || 
-                   (one.number == other.number && one.order > other.order);
-        }
-        static Number getMinusSentinel() {
-            return Number{1000000000, 1000000000};
-        }
-    };
+    Number(int number, Heap *heap_ptr, int heap_idx) :
+        number(number),
+        heap_ptr(heap_ptr),
+        heap_idx(heap_idx) {}
 };
 
-template<typename TBinaryHeap>
-void removeExpired(int order, TBinaryHeap *heap) {
-    while (heap->getSize() > 0 && heap->getTop().order < order) {
-        heap->removeTop();
+bool comparatorLess(Number *one, Number *other) {
+    return (one->number < other->number) || 
+           (one->number == other->number && one->heap_idx < other->heap_idx);
+}
+
+bool comparatorGreat(Number *one, Number *other) {
+    return (one->number > other->number) || 
+           (one->number == other->number && one->heap_idx > other->heap_idx);
+}
+
+Number *getMinusSentinel() {
+    static Number number(-1000000, nullptr, -100000);
+    return &number;
+}
+
+Number *getSentinel() {
+    static Number number(100000, nullptr, 100000);
+    return &number;
+}
+
+void checkHeap(BinaryHeap<Number *> *heap) {
+    for (int i = 0; i < heap->getSize(); ++i) {
+        assert((*heap)[i]->heap_ptr == heap);
+        assert((*heap)[i]->heap_idx == i);
     }
-};
+}
 
-void kOrderStatistics(int k, const std::vector<int> &array, const std::vector<char> &moves, std::vector<int> *result) {
-    BinaryHeap<Number, Number::ComparatorLess> less_heap(k);
-    BinaryHeap<Number, Number::ComparatorGreat> greater_heap(array.size());
+void swap_(Number *&one, Number *&two) {
+    std::swap(one, two);
+    std::swap(one->heap_idx, two->heap_idx);
+}
+
+void insertHeap(BinaryHeap<Number *> *heap, Number *elem) {
+    elem->heap_idx = heap->getSize();
+    int idx = heap->insert(elem);
+    (*heap)[idx]->heap_ptr = heap;
+    (*heap)[idx]->heap_idx = idx;
+}
+
+void kOrderStatistics(int k, 
+                      const std::vector<int> &array, 
+                      const std::vector<char> &moves, std::vector<int> *result) {
 
     int left = 0, right = 0;
-    std::vector<int> less_heap_indexes(array.size());
-    less_heap_indexes[0] = less_heap.insert(Number{array[0], 0});
+    BinaryHeap<Number *> less_heap(k, comparatorLess, getMinusSentinel); // max
+    BinaryHeap<Number *> greater_heap(array.size(), comparatorGreat, getSentinel); // min
+    std::vector<Number *> tmp_elements(array.size(), nullptr);
+    tmp_elements[0] = new Number(array[0], &less_heap, 0);
+    insertHeap(&less_heap, tmp_elements[0]);
 
     for (auto it = moves.begin(); it < moves.end(); ++it) {
         if (*it == 'L') {
             left++;
-            int idx = less_heap_indexes[left - 1];
-            // bad array
-            less_heap.remove(idx);
 
-            removeExpired(left, &less_heap);
-            removeExpired(left, &greater_heap);
-
+            Number *rem_elem = tmp_elements[left - 1];
+            BinaryHeap<Number *> *t_heap = rem_elem->heap_ptr;
+            int rem_idx = t_heap->remove(rem_elem->heap_idx);
+            (*t_heap)[rem_idx] = nullptr;
+    
             if(less_heap.getSize() < k && greater_heap.getSize() > 0) {
-                less_heap.insert(greater_heap.removeTop());
+                int gt_idx = greater_heap.removeTop();
+                Number *element = greater_heap[gt_idx];
+                greater_heap[gt_idx] = nullptr;
+
+                insertHeap(&less_heap, element);
             }
         }
         else if (*it == 'R') {
             right++;
-            Number elem{array[right], right};
-            Number::ComparatorLess comparator;
+            Number *elem = new Number(array[right], nullptr, 0);
+            tmp_elements[right] = elem;
 
             if(less_heap.getSize() < k) {
-                int idx = less_heap.insert(elem);
-                less_heap_indexes[right] = idx;
+                insertHeap(&less_heap, elem);
 
-            } else if (comparator(less_heap.getTop(), elem)) {
-                greater_heap.insert(elem);
-
+            } else if (comparatorLess(less_heap.getTop(), elem)) {
+                insertHeap(&greater_heap, elem);
             } else {
-                Number max_left = less_heap.removeTop();
-                greater_heap.insert(max_left);
-                int idx = less_heap.insert(elem);
-                less_heap_indexes[right] = idx;
+                Number *max_lt = less_heap.getTop();
+                int max_lt_idx = less_heap.removeTop();
+                less_heap[max_lt_idx] = nullptr;
+
+                insertHeap(&greater_heap, max_lt);
+                insertHeap(&less_heap, elem);
             } 
         }
-        result->push_back(less_heap.getSize() < k ? -1 : less_heap.getTop().number);
+        result->push_back(less_heap.getSize() < k ? -1 : less_heap.getTop()->number);
+
+        checkHeap(&less_heap);
+        checkHeap(&greater_heap);
     }
 }
-
 
 void readInput(int *k, std::vector<int> *vector, std::vector<char> *moves) {
     int vector_size, moves_number;
