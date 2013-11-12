b162cd97030eb5c024d482815615163f63aaaad5
Tatiana Borisova <tanyatik@yandex.ru>
2013-11-13 01:28:47 +0400

    Small style changes (still does not pass tests :( )

--- sliding_order_statistics_task.cpp
+++ sliding_order_statistics_task.cpp
@@ -9,17 +9,18 @@ class BinaryHeap {
         typedef TElem TElemType;
 
         BinaryHeap(std::size_t max_size, 
-                   std::function <bool(TElem&, TElem&)> comparator,
-                   std::function <TElem()> sentinel);
+                   std::function <bool (TElem&, TElem&)> comparator,
+                   std::function <TElem ()> sentinel);
         TIndex removeTop();
         const TElem& getTop() const;
         TIndex insert(TElem elem);
-        TElem &operator [] (TIndex index);
-        const TElem &operator [] (TIndex index) const;
+        TElem &operator[] (TIndex index);
+        const TElem &operator[] (TIndex index) const;
         TIndex replace(TIndex prev_idx, TElem new_elem);
         TIndex remove(TIndex idx);
         void debugPrint() const;
         unsigned int getSize() const;
+
     private:
         void heapifyDown(TIndex idx);
         TIndex heapifyUp(TIndex idx);
@@ -33,13 +34,13 @@ class BinaryHeap {
         unsigned int heap_size_;
         unsigned int max_heap_size_;
         std::vector<TElem> elems_;
-        std::function <bool(TElem&, TElem&)> comparator_;
+        std::function <bool (TElem&, TElem&)> comparator_;
         std::function <TElem()> sentinel_;
 };
 
 template<typename TElem>
 BinaryHeap<TElem>::BinaryHeap(std::size_t max_size, 
-                              std::function<bool(TElem&, TElem&)> comparator,
+                              std::function<bool (TElem&, TElem&)> comparator,
                               std::function<TElem()> sentinel) : 
     heap_size_(0),
     max_heap_size_(max_size),
@@ -48,13 +49,13 @@ BinaryHeap<TElem>::BinaryHeap(std::size_t max_size,
     sentinel_(sentinel) {}
 
 template<typename TElem>
-const TElem& BinaryHeap<TElem>::getTop() const{
+const TElem& BinaryHeap<TElem>::getTop() const {
     return elems_[0];
 }
 
 template<typename TElem>
 typename BinaryHeap<TElem>::TIndex BinaryHeap<TElem>::getParentIndex(TIndex idx) {
-    if(idx <= 0) return -1;
+    if (idx <= 0) return -1;
     return (idx - 1) >> 1;
 }
 
@@ -80,12 +81,12 @@ unsigned int BinaryHeap<TElem>::getSize() const {
 }
 
 template<typename TElem>
-TElem& BinaryHeap<TElem>::operator [] (TIndex idx) {
+TElem& BinaryHeap<TElem>::operator[] (TIndex idx) {
     return const_cast<TElem&>(static_cast<const BinaryHeap<TElem>&>(*this)[idx]);
 }
 
 template<typename TElem>
-const TElem& BinaryHeap<TElem>::operator [] (TIndex idx) const {
+const TElem& BinaryHeap<TElem>::operator[] (TIndex idx) const {
     return elems_[idx];
 }
 
@@ -155,7 +156,7 @@ void BinaryHeap<TElem>::deleteElement(TIndex idx) {
 
 template<typename TElem>
 void BinaryHeap<TElem>::debugPrint() const {
-    for(auto k_it = elems_.begin(); k_it != elems_.end(); ++k_it) {
+    for (auto k_it = elems_.begin(); k_it != elems_.end(); ++k_it) {
         std::cout << *k_it;
         std::cout << '\t';
     }
@@ -166,7 +167,7 @@ template<typename TElem>
 typename BinaryHeap<TElem>::TIndex BinaryHeap<TElem>::heapifyUp(TIndex idx) {
     TIndex parent_idx = getParentIndex(idx);
 
-    while(idx > 0 && comparator_(elems_[parent_idx], elems_[idx])) {
+    while (idx > 0 && comparator_(elems_[parent_idx], elems_[idx])) {
         swapElements(idx, parent_idx);
         idx = parent_idx;
         parent_idx = getParentIndex(idx);
@@ -200,12 +201,12 @@ bool comparatorGreat(Number *one, Number *other) {
 }
 
 Number *getMinusSentinel() {
-    static Number number(-1000000, nullptr, -100000);
+    static Number number(-1, nullptr, 0);
     return &number;
 }
 
 Number *getSentinel() {
-    static Number number(100000, nullptr, 100000);
+    static Number number(2147483646, nullptr, 2147483646);
     return &number;
 }
 
@@ -221,14 +222,14 @@ void insertHeap(BinaryHeap<Number *> *heap, Number *elem) {
     (*heap)[idx]->heap_idx = idx;
 }
 
-void kOrderStatistics(int k, 
+void kOrderStatistics(int k_order, 
                       const std::vector<int> &array, 
                       const std::vector<char> &moves, std::vector<int> *result) {
 
     int left = 0, right = 0;
-    BinaryHeap<Number *> less_heap(k, comparatorLess, getMinusSentinel); // max
-    BinaryHeap<Number *> greater_heap(array.size(), comparatorGreat, getSentinel); // min
-    std::vector<Number *> tmp_elements(array.size(), nullptr);
+    BinaryHeap<Number *> less_heap(k_order + 1, comparatorLess, getMinusSentinel); // max
+    BinaryHeap<Number *> greater_heap(array.size() + 1, comparatorGreat, getSentinel); // min
+    std::vector<Number *> tmp_elements(array.size() + 1, nullptr);
     tmp_elements[0] = new Number(array[0], &less_heap, 0);
     insertHeap(&less_heap, tmp_elements[0]);
 
@@ -241,7 +242,7 @@ void kOrderStatistics(int k,
             int rem_idx = t_heap->remove(rem_elem->heap_idx);
             (*t_heap)[rem_idx] = nullptr;
     
-            if(less_heap.getSize() < k && greater_heap.getSize() > 0) {
+            if (less_heap.getSize() < k_order && greater_heap.getSize() > 0) {
                 int gt_idx = greater_heap.removeTop();
                 Number *element = greater_heap[gt_idx];
                 greater_heap[gt_idx] = nullptr;
@@ -254,7 +255,7 @@ void kOrderStatistics(int k,
             Number *elem = new Number(array[right], nullptr, 0);
             tmp_elements[right] = elem;
 
-            if(less_heap.getSize() < k) {
+            if (less_heap.getSize() < k_order) {
                 insertHeap(&less_heap, elem);
 
             } else if (comparatorLess(less_heap.getTop(), elem)) {
@@ -268,13 +269,17 @@ void kOrderStatistics(int k,
                 insertHeap(&less_heap, elem);
             } 
         }
-        result->push_back(less_heap.getSize() < k ? -1 : less_heap.getTop()->number);
+        result->push_back(less_heap.getSize() < k_order ? -1 : less_heap.getTop()->number);
+    }
+
+    for (int tidx = 0; tidx < tmp_elements.size(); ++tidx) {
+        delete tmp_elements[tidx];
     }
 }
 
-void readInput(int *k, std::vector<int> *vector, std::vector<char> *moves) {
+void readInput(int *k_order, std::vector<int> *vector, std::vector<char> *moves) {
     int vector_size, moves_number;
-    std::cin >> vector_size >> moves_number >> *k;
+    std::cin >> vector_size >> moves_number >> *k_order;
     vector->resize(vector_size);
     moves->resize(moves_number);
 
@@ -288,14 +293,17 @@ void readInput(int *k, std::vector<int> *vector, std::vector<char> *moves) {
 }
 
 int main() {
-    int k;
+    int k_order;
     std::vector<int> array;
     std::vector<char> moves;
     std::vector<int> result;
     
-    readInput(&k, &array, &moves);
-    kOrderStatistics(k, array, moves, &result);
-    for_each(result.begin(), result.end(), [](int it){std::cout << it << std::endl;});
-
+    try {
+        readInput(&k_order, &array, &moves);
+        kOrderStatistics(k_order, array, moves, &result);
+        for_each(result.begin(), result.end(), [](int it) {std::cout << it << std::endl;});
+    } catch (...) {
+        std::cout << "Exception thrown\n";
+    }
     return 0;
 }
