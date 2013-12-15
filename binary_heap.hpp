bdd6a51978bc597feb9c7450b4cf7b1583d4e165
Tatiana Borisova <tanyatik@yandex.ru>
2013-12-15 20:11:06 +0400

    Improved binary heap according to mlevin review

--- binary_heap.hpp
+++ binary_heap.hpp
@@ -5,25 +5,34 @@
 #include <iostream>
 #include <functional>
 
-template <typename TElement>
+template <typename TElement,
+          typename TComparator,
+          typename MoveCallback>
+class BinaryHeap;
+
+struct EmptyCallback {
+    template<typename TElement, typename TIndex>
+    void operator () (const TElement &, 
+                      TIndex) {}
+};
+
+
+template <typename TElement,
+          typename TComparator = std::less<TElement>,
+          typename MoveCallback = EmptyCallback>
 class BinaryHeap {
     public:
         typedef long long TIndex;
         typedef TElement TElementType;
 
-        explicit BinaryHeap(std::size_t max_size);
-        BinaryHeap(std::size_t max_size, 
-                   std::function<bool(TElement, TElement)> comparator);
-
+        // Removes element on the top of the heap
         void pop();
-        const TElement& getTop() const;
-        
+        const TElement& getTop() const; 
         void insert(const TElement& element);
-
+        // Removes element which is located by index, keeping all heap properties
         void removeElementByIndex (TIndex index);
         
-        void debugPrint() const;
-        const std::vector<TElement>& getElements() const;
+        // Number of elements kept in the heap
         unsigned int getSize() const;
 
     private:
@@ -32,178 +41,124 @@ class BinaryHeap {
         void heapifyDown(TIndex index);
         TIndex heapifyUp(TIndex index);
 
-        TIndex increaseKey(TElement new_element, TIndex index);
         void swapElements(TIndex a, TIndex b);
 
         TIndex getParentIndex(TIndex index);
         TIndex getLeftIndex(TIndex index);
         TIndex getRightIndex(TIndex index);
 
+        // Helper function -- swaps element with index 'index' 
+        // with last element of the heap,
+        // and then removes last element
         void deleteElement(TIndex index);
 
-        unsigned int heap_size_;
-        unsigned int max_heap_size_;
         std::vector<TElement> elements_;
-        std::function<bool(TElement, TElement)> comparator_;
 };
 
-template<typename TElement>
-BinaryHeap<TElement>::BinaryHeap(std::size_t max_size) : 
-    heap_size_(0),
-    max_heap_size_(max_size),
-    elements_(max_heap_size_),
-    comparator_([] (const TElement &one, const TElement &other) {
-        return one < other;
-    }) {}
-
-template<typename TElement>
-BinaryHeap<TElement>::BinaryHeap(std::size_t max_size, 
-                                 std::function<bool(TElement, TElement)> comparator) : 
-    heap_size_(0),
-    max_heap_size_(max_size),
-    elements_(max_heap_size_),
-    comparator_(comparator) {}
-
-template<typename TElement>
-bool BinaryHeap<TElement>::compareElements(const TElement &one, const TElement &other) const {
-    return comparator_(one, other);
+template<typename TElement, typename TComparator, typename TMoveCallback>
+bool BinaryHeap<TElement, TComparator, TMoveCallback>::compareElements(const TElement &one, 
+                                                        const TElement &other) const {
+    return TComparator() (one, other);
 }
 
-template<typename TElement>
-const TElement &BinaryHeap<TElement>::getTop() const {
-    assert(heap_size_ >= 1);
-    
+template<typename TElement, typename TComparator, typename TMoveCallback>
+const TElement &BinaryHeap<TElement, TComparator, TMoveCallback>::getTop() const {
     return elements_[0];
 }
 
-template<typename TElement>
-const std::vector<TElement> &BinaryHeap<TElement>::getElements() const {
-    return elements_;
-}
-
-template<typename TElement>
-typename BinaryHeap<TElement>::TIndex BinaryHeap<TElement>::getParentIndex(TIndex index) {
-    assert(index < max_heap_size_);
-    if(index <= 0) return -1;
+template<typename TElement, typename TComparator, typename TMoveCallback>
+typename BinaryHeap<TElement, TComparator, TMoveCallback>::TIndex BinaryHeap<TElement, TComparator, TMoveCallback>::getParentIndex
+        (TIndex index) {
+    if (index <= 0) return -1;
     return (index - 1) >> 1;
 }
 
-template<typename TElement>
-typename BinaryHeap<TElement>::TIndex BinaryHeap<TElement>::getLeftIndex(TIndex index) {
-    assert(index == 0 || (index > 0 && index < heap_size_));
+template<typename TElement, typename TComparator, typename TMoveCallback>
+typename BinaryHeap<TElement, TComparator, TMoveCallback>::TIndex BinaryHeap<TElement, TComparator, TMoveCallback>::getLeftIndex
+        (TIndex index) {
     return (index << 1) + 1;
 }
 
-template<typename TElement>
-typename BinaryHeap<TElement>::TIndex BinaryHeap<TElement>::getRightIndex(TIndex index) {
-    assert(index == 0 || (index > 0 && index < heap_size_));
+template<typename TElement, typename TComparator, typename TMoveCallback>
+typename BinaryHeap<TElement, TComparator, TMoveCallback>::TIndex BinaryHeap<TElement, TComparator, TMoveCallback>::getRightIndex
+        (TIndex index) {
     return (index << 1) + 2;
 }
 
-template<typename TElement>
-void BinaryHeap<TElement>::swapElements(TIndex a, TIndex b) {
-    assert(a >= 0 && a < max_heap_size_);
-    assert(b >= 0 && b < max_heap_size_);
+template<typename TElement, typename TComparator, typename TMoveCallback>
+void BinaryHeap<TElement, TComparator, TMoveCallback>::swapElements(TIndex a, TIndex b) {
     using std::swap;
     swap(elements_[a], elements_[b]);
+    TMoveCallback()(elements_[a], a);
+    TMoveCallback()(elements_[b], b);
 }
 
-template<typename TElement>
-unsigned int BinaryHeap<TElement>::getSize() const {
-    assert(heap_size_ <= max_heap_size_);
-    return heap_size_;
+template<typename TElement, typename TComparator, typename TMoveCallback>
+unsigned int BinaryHeap<TElement, TComparator, TMoveCallback>::getSize() const {
+    return elements_.size();
 }
 
-template<typename TElement>
-void BinaryHeap<TElement>::heapifyDown(TIndex index) {
-    assert(index == 0 || (index > 0 && index < heap_size_));
-
+template<typename TElement, typename TComparator, typename TMoveCallback>
+void BinaryHeap<TElement, TComparator, TMoveCallback>::heapifyDown(TIndex index) {
     TIndex left_index = getLeftIndex(index);
     TIndex right_index = getRightIndex(index);
     TIndex max_index = index;
 
-    if (left_index < heap_size_ && compareElements(elements_[max_index], elements_[left_index])) {
+    if (left_index < getSize() && compareElements(elements_[max_index], elements_[left_index])) {
         max_index = left_index;
     }
-    if (right_index < heap_size_ && compareElements(elements_[max_index], elements_[right_index])) {
+    if (right_index < getSize() && compareElements(elements_[max_index], elements_[right_index])) {
         max_index = right_index;
     }
     if (max_index != index) {
         swapElements(index, max_index);
-        assert(max_index >= 0 && max_index < heap_size_);
         heapifyDown(max_index);
     }
 }
 
-template<typename TElement>
-void BinaryHeap<TElement>::insert(const TElement &element) {
-    assert(heap_size_ + 1 <= max_heap_size_);
+template<typename TElement, typename TComparator, typename TMoveCallback>
+void BinaryHeap<TElement, TComparator, TMoveCallback>::insert(const TElement &element) {
+    elements_.push_back(element);
 
-    elements_[heap_size_] = element;
-    TIndex new_index = heapifyUp(heap_size_);
-    assert(new_index >= 0 && new_index <= heap_size_);
+    int heap_size = elements_.size() - 1;
+    elements_[heap_size] = element;
+    TMoveCallback()(elements_[heap_size], heap_size);
 
-    ++heap_size_;
+    heapifyUp(heap_size);
 }
 
-template<typename TElement>
-void BinaryHeap<TElement>::pop() {
-    assert(heap_size_ >= 1);
-    
+template<typename TElement, typename TComparator, typename TMoveCallback>
+void BinaryHeap<TElement, TComparator, TMoveCallback>::pop() {
     deleteElement(0);
     heapifyDown(0);
 }
 
-template<typename TElement>
-void BinaryHeap<TElement>::removeElementByIndex(TIndex index) {
-    assert(index >= 0 && index < heap_size_);
-    assert(heap_size_ >= 1);
-    
+template<typename TElement, typename TComparator, typename TMoveCallback>
+void BinaryHeap<TElement, TComparator, TMoveCallback>::removeElementByIndex(TIndex index) {
     deleteElement(index);
     
-    if (index < heap_size_) {
+    if (index < getSize()) {
         index = heapifyUp(index);
-        assert(index >= 0 && index <= heap_size_);
         heapifyDown(index);
     }
 }
 
-template<typename TElement>
-typename BinaryHeap<TElement>::TIndex BinaryHeap<TElement>::increaseKey(TElement new_element, TIndex index) {
-    assert(index >= 0 && index < max_heap_size_);
-    assert(compareElements(elements_[index], new_element));
-
-    elements_[index] = new_element;
-    index = heapifyUp(index);
-    return index;
+template<typename TElement, typename TComparator, typename TMoveCallback>
+void BinaryHeap<TElement, TComparator, TMoveCallback>::deleteElement(TIndex index) {
+    swapElements(index, getSize() - 1);
+    elements_.erase(elements_.end() - 1);
+    //elements_[getSize() - 1] = TElement();
 }
 
-template<typename TElement>
-void BinaryHeap<TElement>::deleteElement(TIndex index) {
-    swapElements(index, heap_size_ - 1);
-    elements_[heap_size_ - 1] = TElement();
-    --heap_size_;
-}
-
-template<typename TElement>
-typename BinaryHeap<TElement>::TIndex BinaryHeap<TElement>::heapifyUp(TIndex index) {
+template<typename TElement, typename TComparator, typename TMoveCallback>
+typename BinaryHeap<TElement, TComparator, TMoveCallback>::TIndex BinaryHeap<TElement, TComparator, TMoveCallback>::heapifyUp
+            (TIndex index) {
     TIndex parent_index = getParentIndex(index);
 
-    while(index > 0 && compareElements(elements_[parent_index], elements_[index])) {
+    while (index > 0 && compareElements(elements_[parent_index], elements_[index])) {
         swapElements(index, parent_index);
         index = parent_index;
         parent_index = getParentIndex(index);
     }
     return index;
 }
-
-template<typename TElement>
-void BinaryHeap<TElement>::debugPrint() const {
-    for(auto element = elements_.begin(); element != elements_.end(); ++element) {
-        element.debugPrint();
-        std::cout << '\t';
-    }
-    std::cout << "\n";
-}
-
-
--- ut/binary_heap_ut.cpp
+++ ut/binary_heap_ut.cpp
@@ -4,47 +4,9 @@
 
 typedef BinaryHeap<int> TestHeap;
 
-void testHeap(const TestHeap& heap, 
-              std::vector<typename TestHeap::TElementType> expected_elements) {
-    auto heap_elements = heap.getElements();
 
-    ASSERT_TRUE(heap.getSize() <= expected_elements.size());
-    ASSERT_EQ(expected_elements.size(), heap_elements.size());
-
-
-    for(size_t i = 0; i < heap.getSize(); ++i) {
-        ASSERT_EQ(heap_elements[i], expected_elements[i]);
-    }
-}
-
-TEST(heap_test, insertions) {
-    TestHeap f(13);
-    f.insert(20);
-    testHeap(f, {20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
-    f.insert(15);
-    testHeap(f, {20, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
-    f.insert(11);
-    testHeap(f, {20, 15, 11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
-    f.insert(6);
-    testHeap(f, {20, 15, 11, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0});
-    f.insert(9);
-    testHeap(f, {20, 15, 11, 6, 9, 0, 0, 0, 0, 0, 0, 0, 0});
-    f.insert(1);
-    testHeap(f, {20, 15, 11, 6, 9, 1, 0, 0, 0, 0, 0, 0, 0});
-    f.insert(3);
-    testHeap(f, {20, 15, 11, 6, 9, 1, 3, 0, 0, 0, 0, 0, 0});
-    f.insert(5);
-    testHeap(f, {20, 15, 11, 6, 9, 1, 3, 5, 6, 0, 0, 0, 0});
-    f.insert(17);
-    testHeap(f, {20, 17, 11, 15, 9, 1, 3, 5, 6, 0, 0, 0, 0});
-    f.insert(7);
-    testHeap(f, {20, 17, 11, 15, 9, 1, 3, 5, 6, 7, 0, 0, 0});
-    f.insert(8);
-    testHeap(f, {20, 17, 11, 15, 9, 1, 3, 5, 6, 7, 8, 0, 0});
-}
-
-TEST(heap_test, extract_max) {
-    TestHeap h(13);
+TEST(heap_test, insert_extract_max) {
+    TestHeap h;
     h.insert(20);
     h.insert(15);
     h.insert(11);
@@ -57,50 +19,38 @@ TEST(heap_test, extract_max) {
     h.insert(7);
     h.insert(8);
 
-    testHeap(h, {20, 17, 11, 15, 9, 1, 3, 5, 6, 7, 8, 0, 0});
     ASSERT_EQ(h.getTop(), 20);
     h.pop();
-    testHeap(h, {17, 15, 11, 8, 9, 1, 3, 5, 6, 7, 0, 0, 0});
     ASSERT_EQ(h.getTop(), 17);
     h.pop();
-    testHeap(h, {15, 9, 11, 8, 7, 1, 3, 5, 6, 0, 0, 0, 0});
     ASSERT_EQ(h.getTop(), 15);
     h.pop();
-    testHeap(h, {11, 9, 6, 8, 7, 1, 3, 5, 0, 0, 0, 0, 0});
     ASSERT_EQ(h.getTop(), 11);
     h.pop();
-    testHeap(h, {9, 8, 6, 5, 7, 1, 3, 0, 0, 0, 0, 0, 0});
     ASSERT_EQ(h.getTop(), 9);
     h.pop();
-    testHeap(h, {8, 7, 6, 5, 3, 1, 0, 0, 0, 0, 0, 0, 0});
     ASSERT_EQ(h.getTop(), 8);
     h.pop();
-    testHeap(h, {7, 5, 6, 1, 3, 0, 0, 0, 0, 0, 0, 0, 0});
     ASSERT_EQ(h.getTop(), 7);
     h.pop();
-    testHeap(h, {6, 5, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0});
     ASSERT_EQ(h.getTop(), 6);
     h.pop();
-    testHeap(h, {5, 1, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
     ASSERT_EQ(h.getTop(), 5);
     h.pop();
-    testHeap(h, {3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
     ASSERT_EQ(h.getTop(), 3);
     h.pop();
-    testHeap(h, {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
     ASSERT_EQ(h.getTop(), 1);
     h.pop();
     h.insert(20);
-    testHeap(h, {20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
     ASSERT_EQ(h.getTop(), 20);
 }
 
 TEST(heap_test, removals) {
-    TestHeap h(15);
+    TestHeap h;
     h.insert(15);
-    testHeap(h, {15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
+    ASSERT_EQ(h.getTop(), 15);
     h.removeElementByIndex(0);
-    testHeap(h, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
+    ASSERT_EQ(h.getSize(), 0);
     h.insert(100);
     h.insert(25);
     h.insert(75);
@@ -116,12 +66,11 @@ TEST(heap_test, removals) {
     h.insert(53);
     h.insert(48);
     h.insert(44);
-    testHeap(h, {100, 25, 75, 15, 20, 55, 50, 11, 12, 13, 14, 52, 53, 48, 44});
+    ASSERT_EQ(h.getTop(), 100);
     h.removeElementByIndex(1);
-    testHeap(h, {100, 44, 75, 15, 20, 55, 50, 11, 12, 13, 14, 52, 53, 48, 0});
+    ASSERT_EQ(h.getTop(), 100);
     h.removeElementByIndex(7);
-    testHeap(h, {100, 48, 75, 44, 20, 55, 50, 15, 12, 13, 14, 52, 53, 0, 0});
     h.removeElementByIndex(0);
-
+    ASSERT_EQ(h.getTop(), 75);
 }
 
