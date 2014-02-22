975f5cd9153fbc65a5a8763918390209d8f5de3a
Tatiana Borisova <tanyatik@yandex.ru>
2014-02-22 23:51:39 +0400

    Make code more compact

--- prefix.hpp
+++ prefix.hpp
@@ -2,34 +2,6 @@
 #include <vector>
 #include <string>
 
-namespace detail {
-
-inline int computeZFunctionDummy(const std::string string,
-            int index_left,
-            int index_right) {
-
-    int z_function = 0;
-    while(index_right < string.size() && string[index_left] == string[index_right]) {
-        z_function++;
-        index_left++;
-        index_right++;
-    }
-
-    return z_function;
-}
-
-inline void updateZFunctionLongest(const std::vector<int> z_function, 
-            int index,
-            int *longest_begin, 
-            int *longest_end) {
-    if (z_function[index] > 0 && index + z_function[index] > *longest_end) {
-        *longest_begin = index;
-        *longest_end = z_function[index];
-    }
-}
-
-} // namespace detail
-
 namespace tanyatik {
 
 // Computes prefix function for string s
@@ -39,7 +11,7 @@ inline std::vector<int> computePrefix(const std::string &string) {
     prefix[0] = 0;
     int current = 0;
 
-    for (int index = 0; index + 1 < string.size(); ++index) {
+    for (int index = 0; index + 1 < (int) string.size(); ++index) {
         // Fill prefix[i + 1]
         current = prefix[index];
         bool stop = false;
@@ -60,40 +32,34 @@ inline std::vector<int> computePrefix(const std::string &string) {
 }
 
 inline std::vector<int> computeZFunction(const std::string &string) {
-    using detail::computeZFunctionDummy;
-    using detail::updateZFunctionLongest;
-
     std::vector<int> z_function(string.size(), 0);
     
     z_function[0] = string.size();
-    int longest_begin = 0; // start of prefix with biggest value of (prefix_start + prefix_length)
-    int longest_end = 0;
+    int most_right_match_begin = 0; // start of prefix with biggest value of (prefix_start + prefix_length)
+    int most_right_match_end = 0;
 
-    for (int index = 1; index < string.size(); ++index) {
+    for (int index = 1; index < (int) string.size(); ++index) {
         // index belongs to [prefix_start, prefix_start + prefix_length]
-        if (longest_end > index) {
-            // string[index_mirror] = string[index] in the beginning of string
-            int index_mirror = index - longest_begin;
+        if (most_right_match_end > index) {
+            z_function[index] = std::min(z_function[index - most_right_match_begin], 
+                most_right_match_end - index + 1);
+        }    
 
-            int length = z_function[longest_begin] - index_mirror; 
-            
-            if (z_function[index_mirror] < length) {
-                // can use previous value
-                z_function[index] = z_function[index_mirror];
-            } else {
-                // should count
-                z_function[index] = length;
+        int index_left = z_function[index];
+        int index_right = index + index_left;
 
-                z_function[index] = length + 
-                    computeZFunctionDummy(string, index_mirror + length, index + length);
-                updateZFunctionLongest(z_function, index, &longest_begin, &longest_end);
-            }       
-        } else {
-            z_function[index] = computeZFunctionDummy(string, 0, index);
-            updateZFunctionLongest(z_function, index, &longest_begin, &longest_end);
+        while (index_right < (int) string.size() && string[index_left] == string[index_right]) {
+            z_function[index]++;
+            index_left++;
+            index_right++;
+        }
+
+        if (z_function[index] > 0 && index + z_function[index] > most_right_match_end) {
+            most_right_match_begin = index;
+            most_right_match_end = index + z_function[index];
         }
     }
-
+    
     return std::move(z_function);
 }
 
