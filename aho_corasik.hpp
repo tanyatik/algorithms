e6c85779f714f514837fd2dd1d850fd06acc3161
Tatiana Borisova <tanyatik@yandex.ru>
2014-03-30 19:54:54 +0400

    Remove std::map (part 1)

--- aho_corasik.hpp
+++ aho_corasik.hpp
@@ -10,52 +10,69 @@ template<class TValue>
 class Trie {
 public:
     struct NodeContent;
-    typedef std::shared_ptr<NodeContent> Node;
+    typedef int Node;
     typedef std::vector<TValue> Value;
-    typedef std::map<char, Node> Transitions;
+    typedef std::vector<Node> Transitions;
+
+    enum { 
+        NO_TRANSITION = -1, 
+        TRANSITION_COUNT = 30,
+        MIN_SYMBOL = 'a', 
+        MAX_SYMBOL = MIN_SYMBOL + TRANSITION_COUNT 
+    };
 
     struct NodeContent {
     public:
-        void addValue(TValue value) {
-            value_.push_back(value);
-        }
-
-        bool hasValue() const {
-            return value_.size() != 0;
-        }
-
-        const Value &getValue() {
-            return value_;
-        }
-
-        bool hasTransition(char symbol) const {
-            return transitions_.find(symbol) != transitions_.end();
-        }
-
-        const Node getTransition(char symbol) const {
-            return transitions_.at(symbol);
-        }
-
-        void addTransition(char symbol, Node transition) {
-            transitions_[symbol] = transition;
-        }
-
-        const Transitions &getTransitions() const {
-            return transitions_;
-        }
-    
-    private:
         Value value_;
         Transitions transitions_;
+
+        NodeContent() :
+            value_(),
+            transitions_(30) {
+            std::fill(transitions_.begin(), transitions_.end(), NO_TRANSITION);
+        }
     };
 
+    void addValue(Node node, TValue value) {
+        nodes_[node].value_.push_back(value);
+    }
+
+    bool hasValue(Node node) const {
+        return nodes_[node].value_.size() != 0;
+    }
+
+    const Value &getValue(Node node) {
+        return nodes_[node].value_;
+    }
+
+    bool hasTransition(Node node, char symbol) const { 
+        assert(node != NO_TRANSITION);
+        assert(symbol - MIN_SYMBOL >= 0 && symbol - MIN_SYMBOL < 30);
+        const auto &transitions = nodes_[node].transitions_;
+
+        return transitions.at(symbol - MIN_SYMBOL) != NO_TRANSITION;
+    }
+
+    const Node getTransition(Node node, char symbol) const {
+        assert(symbol - MIN_SYMBOL >= 0 && symbol - MIN_SYMBOL < 30);
+
+        return nodes_[node].transitions_.at(symbol - MIN_SYMBOL);
+    }
+
+    void addTransition(Node node, char symbol, Node transition) {
+        assert(node != NO_TRANSITION);
+        nodes_[node].transitions_[symbol - MIN_SYMBOL] = transition;
+    }
+
     Trie() :
+        nodes_(),
         root_(createEmptyNode()),
         keys_counter_(0)
         {}
 
-    Node createEmptyNode() const {
-        return std::make_shared<NodeContent>(NodeContent());
+    Node createEmptyNode() {
+        nodes_.emplace_back(NodeContent());
+        return nodes_.size() - 1;
     }
 
     bool isRoot(Node node) const {
@@ -66,24 +83,28 @@ public:
         return root_;
     }
 
+    size_t getNodesCount() const {
+        return nodes_.size();
+    }
+
     void addKey(const std::string &key, const TValue &value) {
         Node current_node = root_;
         auto current_symbol = key.begin();
  
         while (current_symbol != key.end() &&
-               current_node->hasTransition(*current_symbol)) {
-            current_node = current_node->getTransition(*current_symbol);
+               hasTransition(current_node, *current_symbol)) {
+            current_node = getTransition(current_node, *current_symbol);
             ++current_symbol;
         }
    
         while (current_symbol != key.end()) {
-            current_node->addTransition(*current_symbol, createEmptyNode());
-            current_node = current_node->getTransition(*current_symbol);
+            addTransition(current_node, *current_symbol, createEmptyNode());
+            current_node = getTransition(current_node, *current_symbol);
             ++current_symbol;
         }
         
         if (current_symbol == key.end()) {
-            current_node->addValue(value);
+            addValue(current_node, value);
         }
     }
 
@@ -92,37 +113,41 @@ public:
         auto current_symbol = key.begin();        
 
         while (current_symbol != key.end() &&
-               current_node->hasTransition(*current_symbol)) {
-            current_node = current_node->getTransition(*current_symbol);
+               hasTransition(current_node, *current_symbol)) {
+            current_node = getTransition(current_node, *current_symbol);
             ++current_symbol;
         }
     
-        return (current_symbol == key.end() && current_node->hasValue());
+        return (current_symbol == key.end() && hasValue(current_node));
     }
 
 private:
+    std::vector<NodeContent> nodes_;
     Node root_;
+
     size_t keys_counter_;
 };
 
 
 class PatternsMatcher {
-private:
+protected:
     typedef size_t Match;
     typedef Trie<Match> PatternTrie;
 
 public:
+
     std::vector<std::vector<size_t>> searchPatterns(std::string string) const {
         std::vector<std::vector<size_t>> matches(pattern_count_);
         PatternTrie::Node node = trie_.getRoot();
 
         for (size_t index = 0; index < string.size(); ++index) {
-            while (getGotoTransition(node, string[index]) == nullptr) {
+            char symbol = string[index];
+            while (getGotoTransition(node, symbol) == PatternTrie::NO_TRANSITION) {
                 node = getFailTransition(node);
             }
 
-            node = getGotoTransition(node, string[index]);
-            
+            node = getGotoTransition(node, symbol);
+
             for (auto match: getPatternMatches(node)) {
                 matches[match].push_back(index);
             }
@@ -137,6 +162,21 @@ public:
         initAutomata(); 
     }
 
+protected:
+    PatternTrie::Node getTransition(PatternTrie::Node node, char symbol) const {
+        if (getGotoTransition(node, symbol) == PatternTrie::NO_TRANSITION) {
+            while (getGotoTransition(node, symbol) == PatternTrie::NO_TRANSITION) {
+                node = getFailTransition(node);
+            }
+
+            node = getGotoTransition(node, symbol);
+            return node;
+        } else {
+            return getGotoTransition(node, symbol);
+        }
+    }
+
+/*
     size_t getDebugStateNumber(PatternTrie::Node node, 
             std::map<PatternTrie::Node, size_t> *node_numbers, 
             size_t *node_counter) {
@@ -157,34 +197,35 @@ public:
         node_numbers[queue.front()] = node_counter;
 
         while (!queue.empty()) {
-            auto current_state = queue.front();
-            size_t current_state_number = 
-                getDebugStateNumber(current_state, &node_numbers, &node_counter);
+            auto node = queue.front();
+            size_t node_number = 
+                getDebugStateNumber(node, &node_numbers, &node_counter);
             queue.pop();
 
-            std::cout << current_state_number << "\tfail: " <<
-                getDebugStateNumber(getFailTransition(current_state), &node_numbers, &node_counter);
+            std::cout << node_number << "\tfail: " <<
+                getDebugStateNumber(getFailTransition(node), &node_numbers, &node_counter);
 
             std::cout << "\tgoto: ";
 
-            for (const auto &transition: current_state->getTransitions()) {
+            for (const auto &transition: node->getTransitions()) {
                 auto symbol = transition.first;
                 auto state = transition.second; 
 
                 size_t state_number = getDebugStateNumber(state, &node_numbers, &node_counter);
 
-                std::cout << " { " << symbol << " -> " << state_number << " } ";
+                std::cout << " { " << symbol << " -> " << state << " } ";
                 queue.push(state);
             }
             std::cout << "\tmatches: { ";
-            for (const auto &match: pattern_matches_[current_state]) { 
+            for (const auto &match: pattern_matches_[node]) { 
                 std::cout << match << " ";
             }
             std::cout << " } ";
+            std::cout << "is terminal: " << isTerminalState(node);
             std::cout << std::endl;
         }
     }
-
+*/
 private: 
     void initTrie(const std::vector<std::string> &patterns) {
         for (size_t pattern_index = 0; pattern_index < patterns.size(); ++pattern_index) {
@@ -198,45 +239,72 @@ private:
         fail_transitions_[root] = root;
         addPatternMatches(root, std::vector<size_t>()); // in case there are empty patterns
 
-        for (const auto &node: trie_.getRoot()->getTransitions()) {
-            auto state = node.second; 
+        for (size_t symbol = PatternTrie::MIN_SYMBOL; symbol < PatternTrie::MAX_SYMBOL; ++symbol) {
+            if (trie_.hasTransition(trie_.getRoot(), symbol)) {
+                auto node = trie_.getTransition(trie_.getRoot(), symbol);
     
-            fail_transitions_[state] = root;
-            addPatternMatches(state, std::vector<size_t>());
+                fail_transitions_[node] = root;
+                addPatternMatches(node, getPatternMatches(root));
 
-            queue.push(state);
+                queue.push(node);
+            }
         }
 
         while (!queue.empty()) {
             auto current_node = queue.front();
             queue.pop();
             
-            for (const auto &transition: current_node->getTransitions()) {
-                auto symbol = transition.first;
-                auto transition_from = transition.second;
+            for (size_t symbol = PatternTrie::MIN_SYMBOL; symbol != PatternTrie::MAX_SYMBOL; ++symbol) {
+                if (trie_.hasTransition(current_node, symbol)) {
+                    auto transition_from = trie_.getTransition(current_node, symbol);
 
-                queue.push(transition_from);
-                auto transition_to = getFailTransition(current_node);
+                    queue.push(transition_from);
+                    auto transition_to = getFailTransition(current_node);
 
-                while (getGotoTransition(transition_to, symbol) == nullptr) {
-                    transition_to = getFailTransition(transition_to);
+                    while (getGotoTransition(transition_to, symbol) == PatternTrie::NO_TRANSITION) {
+                        transition_to = getFailTransition(transition_to);
+                    }
+
+                    fail_transitions_[transition_from] = getGotoTransition(transition_to, symbol);
+                    addPatternMatches(transition_from, 
+                            getPatternMatches(fail_transitions_[transition_from]));
                 }
-
-                fail_transitions_[transition_from] = getGotoTransition(transition_to, symbol);
-                addPatternMatches(transition_from, 
-                        getPatternMatches(fail_transitions_[transition_from]));
             }
         }
     }
 
+    void addPatternMatches(PatternTrie::Node node, const std::vector<size_t> &implicit_matches) {
+        std::vector<size_t> matches = implicit_matches;
+
+        if (trie_.hasValue(node)) {
+            const auto &values = trie_.getValue(node);
+            matches.insert(matches.begin(), values.begin(), values.end());
+        }
+
+        pattern_matches_[node] = matches;
+    } 
+
+protected: PatternTrie::Node getRoot() const {
+        return trie_.getRoot();
+    }
+
+    bool isTerminalState(PatternTrie::Node node) const {
+        auto found = pattern_matches_.find(node);
+        if (found != pattern_matches_.end()) {
+            return (found->second.size() > 0);
+        }
+        
+        return false;
+    }
+
     PatternTrie::Node getGotoTransition(PatternTrie::Node node, char symbol) const {
-        if (node->hasTransition(symbol)) {
-            return node->getTransition(symbol);
+        if (trie_.hasTransition(node, symbol)) {
+            return trie_.getTransition(node, symbol);
         } else {
             if (trie_.isRoot(node)) {
                 return node;
             } else {
-                return nullptr;   
+                return PatternTrie::NO_TRANSITION;   
             }
         }
     }
@@ -250,17 +318,6 @@ private:
         }
     }
 
-    void addPatternMatches(PatternTrie::Node node, const std::vector<size_t> &implicit_matches) {
-        std::vector<size_t> matches = implicit_matches;
-
-        if (node->hasValue()) {
-            const auto &values = node->getValue();
-            matches.insert(matches.begin(), values.begin(), values.end());
-        }
-
-        pattern_matches_[node] = matches;
-    } 
-
     std::vector<size_t> getPatternMatches(PatternTrie::Node node) const {
         auto found = pattern_matches_.find(node);
         if (found != pattern_matches_.end()) {
@@ -270,12 +327,59 @@ private:
         return std::vector<size_t>();
     }
 
+    size_t getNodesCount() const {
+        return trie_.getNodesCount();
+    }
+
+private:
     PatternTrie trie_;
 
     std::map<PatternTrie::Node, PatternTrie::Node> goto_transitions_;
     std::map<PatternTrie::Node, PatternTrie::Node> fail_transitions_;
-    std::map<PatternTrie::Node, std::set<size_t>> pattern_matches_;
+    std::map<PatternTrie::Node, std::vector<size_t>> pattern_matches_;
 
     int pattern_count_;
 };
 
+class StringsCounter : public PatternsMatcher {
+private:
+    typedef std::map<PatternTrie::Node, long long int> DynamicTableLevel;
+    typedef std::vector<DynamicTableLevel> DynamicTable;
+
+public:
+    static const int MODULUS = 1000000007;
+
+    long long int countExcludedStrings(size_t max_word_length, 
+            size_t alphabet_size) {
+        DynamicTable table(max_word_length + 1);
+         
+        size_t nodes_count = getNodesCount();
+        
+        for (size_t node = 0; node < nodes_count; ++node) {
+            if (isTerminalState(node)) {
+                for (size_t word_length = 0; word_length <= max_word_length; ++word_length) {
+                    table[word_length][node] = 0;
+                }
+            } else {
+                table[0][node] = 1;
+            }
+        }  
+
+        for (size_t word_length = 1; word_length <= max_word_length; ++word_length) {
+            for (size_t node = 0; node < nodes_count; ++node) {
+                if (!isTerminalState(node)) {
+                    long long int result = 0;
+
+                    for (char symbol_index = 0; symbol_index < alphabet_size; ++symbol_index) {
+                        result += table[word_length - 1][getTransition(node, 'a' + symbol_index)];
+                        result %= MODULUS;
+                    }
+
+                    table[word_length][node] = result;
+                }
+            } 
+        }
+
+        return table[max_word_length][getRoot()];
+    }
+};
