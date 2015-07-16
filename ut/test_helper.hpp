f4793ddfe3dec8ffb19b00a5b55c2525acafb1e7
Tatiana Borisova <tanyatik@yandex.ru>
2015-07-16 17:20:43 +0300

    Add test_helper

---
+++ ut/test_helper.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <vector>
+#include <random>
+
+namespace algorithms {
+
+std::vector<int> InitRandomVector(std::default_random_engine *generator,
+        int min_element,
+        int max_element) {
+    const int MAX_ARRAY_SIZE = 10;
+    std::vector<int> vector;
+    for (int i = 0; i < MAX_ARRAY_SIZE; ++i) {
+        vector.push_back(std::normal_distribution<int>(min_element, max_element)(*generator));
+    }
+
+    return vector;
+}
+
+template<typename T>
+std::vector<std::vector<T> > GenerateData(size_t threads_count, size_t elements_count) {
+    std::vector<std::vector<T> > data(threads_count, std::vector<T> (elements_count));
+    std::default_random_engine generator;
+
+    for (auto &thread_data: data) {
+        for (auto &data_element: thread_data) {
+            data_element = std::uniform_int_distribution<T>(0, 100)(generator);
+        }
+    }
+
+    return std::move(data);
+}
+
+template<typename T>
+inline void TestVector(std::vector<T> expected, std::vector<T> result) {
+    ASSERT_EQ(expected.size(), result.size());
+    for (size_t i = 0; i < expected.size(); ++i) {
+        ASSERT_EQ(expected[i], result[i]);
+    }
+}
+
+} // namespace algorithms
